817cc1fc6e5212c07028ad0f5203c67a2ab246c7
b2210356157 <b2210356157@cs.hacettepe.edu.tr>
2021-11-05 22:50:42 +0300

    exercises

---
+++ b2210356157_exersizes/exercise1.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+N = int(input("Please write a number "))
+sum_odd=0
+avarage=0
+
+for i in range(N):
+    
+        if i%2 == 1:
+             sum_odd += i
+print("sum of odd numbers",sum_odd)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ b2210356157_exersizes/exercise2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+email=(input("please write your e-mail"))
+if (email.find(".")>0): 
+
+    if (email.find("@")>0):
+     print(email,"is correct")
+else:
+    (email,"is not correct")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
